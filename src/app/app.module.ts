96050de2a8060dfca3ae2bf26f9d881ede050ce5
orange-lili <orange@magce-mac.local>
2021-12-14 16:57:45 +0800

    clear folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './index/app.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ContentModule } from './content/content.module';
